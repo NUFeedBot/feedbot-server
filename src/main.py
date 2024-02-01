0a7885e3ad5951930c8d1c3f31ca8a7742c9b519
Alessandra Simmons <30960626+MixedMatched@users.noreply.github.com>
2024-01-31 20:59:18 -0500

    switch to postgres

--- src/main.py
+++ src/main.py
@@ -1,33 +1,38 @@
-from flask import Flask
-from tinydb import TinyDB, Query
+from flask import Flask, request
+from postgres import Postgres
 
 app = Flask(__name__)
-db = TinyDB('db.json')
+db = Postgres()
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/submission/<id>")
 def submission(id):
-    Entry = Query()
-    submission = db.search(Entry.id == id)
-    if len(submission) == 0:
+    # TODO: SQL sanitization
+    submission = db.one("SELECT * FROM submissions WHERE id='%s'", id)
+    if submission is None:
         return f"<p>Error: Could not find a submission with the given id: {id}</p>"
     else:
-        return f"<code>{submission[0].code}</code>"
+        return f"<code>{submission.code}</code>"
+
 
 @app.route("/entry")
 def receive_entry():
     data = request.args
     if validate(data):
-        db.insert(transform(data))
+        db.run(f"INSERT INTO submissions VALUES ({data.code}, {data.id})")
     return {'data': data}, 200
+
 
 # this will eventually validate that the sender of an entry is us,
 # presumably by using a shared key
 def validate(args):
     return True
+
 
 # this will eventually change the received entry to contain properly
 # formatted data (I think just exchanging NUID or email for an SSO token?)
