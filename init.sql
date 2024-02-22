b236334bb46a84a21ef6afd2b41cbf1143eaaab6
Alessandra Simmons <30960626+MixedMatched@users.noreply.github.com>
2024-02-21 21:47:30 -0500

    better init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE feedbot_dev;
 CREATE USER feedbot_user WITH PASSWORD '111';
+\c feedbot_dev
 GRANT ALL PRIVILEGES ON DATABASE feedbot_dev TO feedbot_user;
+GRANT ALL ON SCHEMA public TO feedbot_user;
